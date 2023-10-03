412be49a306916db1e43f89250185837853614ec
Nicole Geiger <nmgeiger98@gmail.com>
2023-10-03 15:28:02 -0700

    edited requirements file and database initialization due to Werkzeug compatibility error

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-db = SQLAlchemy
+
+db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dev'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -16,4 +18,12 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Pattern, Yarn, Brand
+    #create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database')
