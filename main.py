a4237062c13f3335b53ec8cec2347154e23fb03a
Nicole Geiger <nmgeiger98@gmail.com>
2023-10-05 20:46:02 -0700

    added a base html template and home page

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from easyreader import easypdf
 app = create_app()
 
 if __name__ == '__main__':
-    #app.run(debug=True)
+    app.run(debug=True)
 
     print(easypdf.readPattern("patterns/crochet3.pdf"))
 
